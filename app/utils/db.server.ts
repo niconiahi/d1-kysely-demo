b4798e5e93773e52e54f274f26aef0478f791409
Nicolas Accetta <nicolas.accetta@gmail.com>
2023-05-15 23:33:02 -0300

    refactor: extract instance db into util

---
+++ app/utils/db.server.ts
@@ -0,0 +1,24 @@
+import type { AppLoadContext } from "@remix-run/cloudflare";
+import type { Generated } from "kysely";
+import { Kysely } from "kysely";
+import { D1Dialect } from "kysely-d1";
+
+export interface Env {
+  DB: D1Database;
+}
+
+interface UserTable {
+  id: Generated<number>;
+  company_name: string;
+  contact_name: string;
+}
+
+interface Database {
+  users: UserTable;
+}
+
+export function getDb(context: AppLoadContext) {
+  return new Kysely<Database>({
+    dialect: new D1Dialect({ database: context.DB }),
+  });
+}
